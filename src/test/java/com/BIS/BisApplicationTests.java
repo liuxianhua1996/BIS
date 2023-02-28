a178b166b350bce45d86ba3521ce2364a09fe714
zhicheng <1318873422@qq.com>
2023-02-28 23:10:11 +0800

    1.项目初始化

---
+++ src/test/java/com/BIS/BisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
